2fbbadf54d648a2f664225c50b80edfa16692dce
unknown <penisxuei@gmail.com>
2024-11-04 23:49:34 +0700

    blya

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -33,7 +33,7 @@ export const login = async (req, res) => {
     }
     // Проверка или создание пользователя в базе данных
     let existingUser = await User.findOne({
-      where: { telegramId: user.telegramId },
+      where: { id: user.id },
       include: { model: Role, as: 'role', attributes: ['name'] },
     });
 
