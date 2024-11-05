b9b9035fb3f214c8a7f607d09b52c1ceeaeb6daa
unknown <penisxuei@gmail.com>
2024-11-05 14:35:31 +0700

    tetet11

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -141,7 +141,7 @@ export const addCoins = async (req, res) => {
       console.log(`Updated user data: ${JSON.stringify(user)}`);
       return res.json({ message: 'Coins added successfully', user });
     } else {
-      return res.status(400).json({ message: 'Insufficient energy to add coins'+updatedEnergy+" "+coins });
+      return res.status(400).json({ message: 'Insufficient energy to add coins'+updatedEnergy+" "+req.body });
     }
   } catch (error) {
     console.error('Database error:', error);  
