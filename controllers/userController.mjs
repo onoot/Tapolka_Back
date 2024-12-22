8c32626a19581f1443081c298373ade535e3a4de
unknown <penisxuei@gmail.com>
2024-12-22 17:12:04 +0700

    tesr

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -76,6 +76,7 @@ export const login = async (req, res) => {
         }
       }
     });
+    console.log(Rewarw_Data);
 
     // Обновление энергии пользователя перед отправкой данных клиенту
     const updatedEnergy = await checkAndRegenerateEnergy(existingUser);
