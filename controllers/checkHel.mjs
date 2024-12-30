09f6745d52db5abbe34c8fad502819bd1f0cc828
unknown <penisxuei@gmail.com>
2024-12-30 16:12:15 +0700

    dsds

--- controllers/checkHel.mjs
+++ controllers/checkHel.mjs
@@ -1,11 +1,9 @@
 import sequelize from '../config/database.mjs';
 
 class Check {
-  // Проверка связи с базой данных
   static checkDatabase = async (req, res) => {
     try {
       await sequelize.authenticate();
-      console.log('Database connection is successful.');
       return res.status(200).json({ success: true, message: 'Database connection is successful.' });
     } catch (error) {
       console.error('Database connection failed:', error);
