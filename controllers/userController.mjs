ea78c3c80cde54fbedff33647575bb21dc81a69c
unknown <penisxuei@gmail.com>
2025-01-18 00:26:06 +0700

    fix datbase

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -154,7 +154,7 @@ export const login = async (req, res) => {
 
     const win = existingUser?.win_combo || { status: false, date: null };
     //сопоставить win дату с датой актуальных заданий, если дата актульных заданий на сегодня больше, чем дата победы, то вернуть пустой массив, иначе вернуть win_combo
-    const winCombo = win.date > today ? [] : win.win_combo;
+    const winCombo = win.date > today ? [] : [win.win_combo];
 
     // Формирование объекта ответа
     return res.json({
@@ -170,7 +170,7 @@ export const login = async (req, res) => {
       rank: existingUser.rank,
       benefit: existingUser.benefit,
       key: existingUser.key,
-      combo_daily_tasks: winCombo,
+      combo_daily_tasks: winCombo||[],
       reward: {
         reward: filteredData[0]?.reward || null,
         date: filteredData[0]?.Data || null
